a1bfc2054a7b1de46db560a682b29f4430c769f6
iljuhenson <ilyapavluk24@gmail.com>
2023-09-08 16:26:44 +0200

    Improved init_problem.py script

--- init_problem.py
+++ init_problem.py
@@ -1,10 +1,15 @@
 import sys
 import os
 
-name_as_list = sys.argv[1:]
+name_as_list = sys.argv[1]
 print(name_as_list)
 
-name_as_problem_name_underscored_text = '_'.join(name_as_list)
+name_as_problem_name_underscored_text = ""
+for i in range(len(name_as_list)):
+    if name_as_list[i] == " ":
+        name_as_problem_name_underscored_text += "_"
+    elif name_as_list[i].isalnum():
+        name_as_problem_name_underscored_text += name_as_list[i]
 
 problem_dir_name = name_as_problem_name_underscored_text.lower()
 solution_name = "solution"
